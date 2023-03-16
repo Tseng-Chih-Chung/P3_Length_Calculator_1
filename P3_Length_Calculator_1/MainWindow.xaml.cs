28b190d56794d7f75d89971f4592f7b6c92ef46c
Tseng-Chih-Chung <chiao105031@gmail.com>
2023-03-16 20:22:10 +0800

    進度
    1.完成所有目標單位之換算
    2.清除按鈕能正常能正確運行
    3.確認在我的設備上無錯誤產生並能進行正常運算
    
    ?.運算無法達預期般準確(應是我除數給的範圍太小導致)

--- P3_Length_Calculator_1/MainWindow.xaml.cs
+++ P3_Length_Calculator_1/MainWindow.xaml.cs
@@ -35,5 +35,70 @@ namespace P3_Length_Calculator_1
             txtOZ.Text = string.Format("{0:0.#####}", douMG / 28349.523125);
             txtLB.Text = string.Format("{0:0.#####}", douMG / 453592.37);
         }
+
+        private void txtG_KeyUp(object sender, KeyEventArgs e)
+        {
+            double douG;
+            douG = Convert.ToDouble(txtG.Text);
+            txtMG.Text = string.Format("{0:0.#####}", douG * 1000);
+            txtKG.Text = string.Format("{0:0.#####}", douG / 1000);
+            txtMT.Text = string.Format("{0:0.#####}", douG / 1000000);
+            txtOZ.Text = string.Format("{0:0.#####}", douG / 28.349523125);
+            txtLB.Text = string.Format("{0:0.#####}", douG / 453.59237);
+        }
+
+        private void txtKG_KeyUp(object sender, KeyEventArgs e)
+        {
+            double douKG;
+            douKG = Convert.ToDouble(txtKG.Text);
+            txtMG.Text = string.Format("{0:0.#####}", douKG * 1000000);
+            txtG.Text = string.Format("{0:0.#####}", douKG * 1000);
+            txtMT.Text = string.Format("{0:0.#####}", douKG / 1000);
+            txtOZ.Text = string.Format("{0:0.#####}", douKG / 0.0283495231);
+            txtLB.Text = string.Format("{0:0.#####}", douKG / 0.45359237);
+        }
+
+        private void txtMT_KeyUp(object sender, KeyEventArgs e)
+        {
+            double douMT;
+            douMT = Convert.ToDouble(txtMT.Text);
+            txtMG.Text = string.Format("{0:0.#####}", douMT * 1000000000);
+            txtG.Text = string.Format("{0:0.#####}", douMT * 1000000);
+            txtKG.Text = string.Format("{0:0.#####}", douMT * 1000);
+            txtOZ.Text = string.Format("{0:0.#####}", douMT / 0.0000283495);
+            txtLB.Text = string.Format("{0:0.#####}", douMT / 0.0004535924);
+        }
+
+        private void txtOZ_KeyUp(object sender, KeyEventArgs e)
+        {
+            double douOZ;
+            douOZ = Convert.ToDouble(txtOZ.Text);
+            txtMG.Text = string.Format("{0:0.}", douOZ / 0.00003527396195);
+            txtG.Text = string.Format("{0:0.}", douOZ / 0.035273962);
+            txtKG.Text = string.Format("{0:0.#####}", douOZ / 35.27396195);
+            txtMT.Text = string.Format("{0:0.#####}", douOZ / 35273.96195);
+            txtLB.Text = string.Format("{0:0.#####}", douOZ / 16);
+        }
+
+        private void txtLB_KeyUp(object sender, KeyEventArgs e)
+        {
+            double douLB;
+            douLB = Convert.ToDouble(txtLB.Text);
+            txtMG.Text = string.Format("{0:0.#}", douLB / 0.00000220462262185);
+            txtG.Text = string.Format("{0:0.}", douLB / 0.0022046226);
+            txtKG.Text = string.Format("{0:0.#####}", douLB / 2.2046226218);
+            txtMT.Text = string.Format("{0:0.#####}", douLB / 2204.62262185);
+            txtOZ.Text = string.Format("{0:0.#####}", douLB * 16);
+        }
+
+        private void Clean_Click(object sender, RoutedEventArgs e)
+        {
+            txtMG.Text = "";
+            txtG.Text = "";
+            txtKG.Text = "";
+            txtMT.Text = "";
+            txtOZ.Text = "";
+            txtLB.Text = "";
+        }
     }
 }
