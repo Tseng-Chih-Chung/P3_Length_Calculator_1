bf076003138d91013643a74d67863d12d4abdfec
Tseng-Chih-Chung <chiao105031@gmail.com>
2023-03-26 14:00:01 +0800

    完成(應該確定沒有要修改的了}
    1.讓所有單位的文字輸入框輸入非數字時清除所有單位文字框，並在訊息輸入框輸出錯誤及提醒
    2.能運算時在資訊輸入框提醒運算結果有誤差
    3.把string douInput刪掉 直接用txt??.TEXT進行替換
    4.目前在我的電腦上能成功運行

--- P3_Length_Calculator_1/MainWindow.xaml.cs
+++ P3_Length_Calculator_1/MainWindow.xaml.cs
@@ -25,70 +25,143 @@ namespace P3_Length_Calculator_1
             InitializeComponent();
         }
 
+        double douOutput; // double浮點數型態的douOutput變數
         private void txtMG_KeyUp_1(object sender, KeyEventArgs e)
         {
-            double douMG;
-            douMG = Convert.ToDouble(txtMG.Text);
-            txtG.Text = string.Format("{0:0.#####}", douMG / 1000);
-            txtKG.Text = string.Format("{0:0.#####}", douMG / 1000000);
-            txtMT.Text = string.Format("{0:0.#####}", douMG / 1000000000);
-            txtOZ.Text = string.Format("{0:0.#####}", douMG / 28349.523125);
-            txtLB.Text = string.Format("{0:0.#####}", douMG / 453592.37);
+            if (double.TryParse(txtMG.Text, out douOutput) == true)
+            { 
+                txtG.Text = string.Format("{0:0.#####}", douOutput / 1000);
+                txtKG.Text = string.Format("{0:0.#####}", douOutput / 1000000);
+                txtMT.Text = string.Format("{0:0.#####}", douOutput / 1000000000);
+                txtOZ.Text = string.Format("{0:0.#####}", douOutput / 28349.523125);
+                txtLB.Text = string.Format("{0:0.#####}", douOutput / 453592.37);
+                txtInfo.Text ="運算成功 計算結果有誤差 切勿全盤相信";
+            }
+            else
+            {
+                txtInfo.Text = "錯誤 只能輸入數字";
+                txtMG.Text = "";
+                txtG.Text = "";
+                txtKG.Text = "";
+                txtMT.Text = "";
+                txtOZ.Text = "";
+                txtLB.Text = "";
+            }
         }
 
         private void txtG_KeyUp(object sender, KeyEventArgs e)
         {
-            double douG;
-            douG = Convert.ToDouble(txtG.Text);
-            txtMG.Text = string.Format("{0:0.#####}", douG * 1000);
-            txtKG.Text = string.Format("{0:0.#####}", douG / 1000);
-            txtMT.Text = string.Format("{0:0.#####}", douG / 1000000);
-            txtOZ.Text = string.Format("{0:0.#####}", douG / 28.349523125);
-            txtLB.Text = string.Format("{0:0.#####}", douG / 453.59237);
+            if (double.TryParse(txtG.Text, out douOutput) == true)
+            {
+                txtMG.Text = string.Format("{0:0.#####}", douOutput * 1000);
+                txtKG.Text = string.Format("{0:0.#####}", douOutput / 1000);
+                txtMT.Text = string.Format("{0:0.#####}", douOutput / 1000000);
+                txtOZ.Text = string.Format("{0:0.#####}", douOutput / 28.349523125);
+                txtLB.Text = string.Format("{0:0.#####}", douOutput / 453.59237);
+                txtInfo.Text = "運算成功 計算結果有誤差 切勿全盤相信";
+            }
+            else
+            {
+                txtInfo.Text = "錯誤 只能輸入數字";
+                txtMG.Text = "";
+                txtG.Text = "";
+                txtKG.Text = "";
+                txtMT.Text = "";
+                txtOZ.Text = "";
+                txtLB.Text = "";
+            }
         }
 
         private void txtKG_KeyUp(object sender, KeyEventArgs e)
         {
-            double douKG;
-            douKG = Convert.ToDouble(txtKG.Text);
-            txtMG.Text = string.Format("{0:0.#####}", douKG * 1000000);
-            txtG.Text = string.Format("{0:0.#####}", douKG * 1000);
-            txtMT.Text = string.Format("{0:0.#####}", douKG / 1000);
-            txtOZ.Text = string.Format("{0:0.#####}", douKG / 0.0283495231);
-            txtLB.Text = string.Format("{0:0.#####}", douKG / 0.45359237);
+            if (double.TryParse(txtKG.Text, out douOutput) == true)
+            {
+                txtMG.Text = string.Format("{0:0.#####}", douOutput * 1000000);
+                txtG.Text = string.Format("{0:0.#####}", douOutput * 1000);
+                txtMT.Text = string.Format("{0:0.#####}", douOutput / 1000);
+                txtOZ.Text = string.Format("{0:0.#####}", douOutput / 0.0283495231);
+                txtLB.Text = string.Format("{0:0.#####}", douOutput / 0.45359237);
+                txtInfo.Text = "運算成功 計算結果有誤差 切勿全盤相信";
+            }
+            else
+            {
+                txtInfo.Text = "錯誤 只能輸入數字";
+                txtMG.Text = "";
+                txtG.Text = "";
+                txtKG.Text = "";
+                txtMT.Text = "";
+                txtOZ.Text = "";
+                txtLB.Text = "";
+            }
         }
 
         private void txtMT_KeyUp(object sender, KeyEventArgs e)
         {
-            double douMT;
-            douMT = Convert.ToDouble(txtMT.Text);
-            txtMG.Text = string.Format("{0:0.#####}", douMT * 1000000000);
-            txtG.Text = string.Format("{0:0.#####}", douMT * 1000000);
-            txtKG.Text = string.Format("{0:0.#####}", douMT * 1000);
-            txtOZ.Text = string.Format("{0:0.#####}", douMT / 0.0000283495);
-            txtLB.Text = string.Format("{0:0.#####}", douMT / 0.0004535924);
+            if (double.TryParse(txtMT.Text, out douOutput) == true)
+            {
+                txtMG.Text = string.Format("{0:0.#####}", douOutput * 1000000000);
+                txtG.Text = string.Format("{0:0.#####}", douOutput * 1000000);
+                txtKG.Text = string.Format("{0:0.#####}", douOutput * 1000);
+                txtOZ.Text = string.Format("{0:0.#####}", douOutput / 0.0000283495);
+                txtLB.Text = string.Format("{0:0.#####}", douOutput / 0.0004535924);
+                txtInfo.Text = "運算成功 計算結果有誤差 切勿全盤相信";
+            }
+            else
+            {
+                txtInfo.Text = "錯誤 只能輸入數字";
+                txtMG.Text = "";
+                txtG.Text = "";
+                txtKG.Text = "";
+                txtMT.Text = "";
+                txtOZ.Text = "";
+                txtLB.Text = "";
+            }
         }
 
         private void txtOZ_KeyUp(object sender, KeyEventArgs e)
         {
-            double douOZ;
-            douOZ = Convert.ToDouble(txtOZ.Text);
-            txtMG.Text = string.Format("{0:0.}", douOZ / 0.00003527396195);
-            txtG.Text = string.Format("{0:0.}", douOZ / 0.035273962);
-            txtKG.Text = string.Format("{0:0.#####}", douOZ / 35.27396195);
-            txtMT.Text = string.Format("{0:0.#####}", douOZ / 35273.96195);
-            txtLB.Text = string.Format("{0:0.#####}", douOZ / 16);
+            if (double.TryParse(txtOZ.Text, out douOutput) == true)
+            {
+                txtMG.Text = string.Format("{0:0.}", douOutput / 0.00003527396195);
+                txtG.Text = string.Format("{0:0.}", douOutput / 0.035273962);
+                txtKG.Text = string.Format("{0:0.#####}", douOutput / 35.27396195);
+                txtMT.Text = string.Format("{0:0.#####}", douOutput / 35273.96195);
+                txtLB.Text = string.Format("{0:0.#####}", douOutput / 16);
+                txtInfo.Text = "運算成功 計算結果有誤差 切勿全盤相信";
+            }
+            else
+            {
+                txtInfo.Text = "錯誤 只能輸入數字";
+                txtMG.Text = "";
+                txtG.Text = "";
+                txtKG.Text = "";
+                txtMT.Text = "";
+                txtOZ.Text = "";
+                txtLB.Text = "";
+            }
         }
 
         private void txtLB_KeyUp(object sender, KeyEventArgs e)
         {
-            double douLB;
-            douLB = Convert.ToDouble(txtLB.Text);
-            txtMG.Text = string.Format("{0:0.#}", douLB / 0.00000220462262185);
-            txtG.Text = string.Format("{0:0.}", douLB / 0.0022046226);
-            txtKG.Text = string.Format("{0:0.#####}", douLB / 2.2046226218);
-            txtMT.Text = string.Format("{0:0.#####}", douLB / 2204.62262185);
-            txtOZ.Text = string.Format("{0:0.#####}", douLB * 16);
+            if (double.TryParse(txtLB.Text, out douOutput) == true)
+            {
+                txtMG.Text = string.Format("{0:0.#}", douOutput / 0.00000220462262185);
+                txtG.Text = string.Format("{0:0.}", douOutput / 0.0022046226);
+                txtKG.Text = string.Format("{0:0.#####}", douOutput / 2.2046226218);
+                txtMT.Text = string.Format("{0:0.#####}", douOutput / 2204.62262185);
+                txtOZ.Text = string.Format("{0:0.#####}", douOutput * 16);
+                txtInfo.Text = "運算成功 計算結果有誤差 切勿全盤相信";
+            }
+            else
+            {
+                txtInfo.Text = "錯誤 只能輸入數字";
+                txtMG.Text = "";
+                txtG.Text = "";
+                txtKG.Text = "";
+                txtMT.Text = "";
+                txtOZ.Text = "";
+                txtLB.Text = "";
+            }
         }
 
         private void Clean_Click(object sender, RoutedEventArgs e)
